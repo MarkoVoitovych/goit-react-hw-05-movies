d541069170591a9f3c16a8d8f68247482eb8521d
MarkoVoitovych <110818474+MarkoVoitovych@users.noreply.github.com>
2023-01-04 16:25:56 +0000

    Deploying to gh-pages from @ MarkoVoitovych/goit-react-hw-05-movies@60b875289abe505e06a0224f3ced99b845872480 🚀

---
+++ static/js/213.c062095f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[213],{1213:function(t,e,n){n.d(e,{FuB:function(){return i}});var r=n(9983);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 15 15",fill:"none"},child:[{tag:"path",attr:{d:"M9.875 7.5C9.875 8.81168 8.81168 9.875 7.5 9.875C6.18832 9.875 5.125 8.81168 5.125 7.5C5.125 6.18832 6.18832 5.125 7.5 5.125C8.81168 5.125 9.875 6.18832 9.875 7.5Z",fill:"currentColor"}}]})(t)}}}]);
+//# sourceMappingURL=213.c062095f.chunk.js.map
